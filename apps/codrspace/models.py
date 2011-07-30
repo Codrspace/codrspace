ee3095bca9331347350acfd9ef187af8331ae2cb
Eloy Zuniga Jr <eloyz.email@gmail.com>
2011-07-30 18:35:09 -0500

    More styling profiling.

--- apps/codrspace/models.py
+++ apps/codrspace/models.py
@@ -3,10 +3,17 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
     title = models.CharField(max_length=200, blank=True)
     content = models.TextField()
     slug = models.SlugField(editable=False)
     author = models.ForeignKey(User, editable=False)
+    status = models.CharField(max_length=30, choices=STATUS_CHOICES, default=0)
     create_dt = models.DateTimeField(auto_now_add=True)
     update_dt = models.DateTimeField(auto_now=True)
 
