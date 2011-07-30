b3c4bd91414605525e0da297f85e62f53a9c389e
Luke Lee <durdenmisc@gmail.com>
2011-07-29 23:13:47 -0500

    Add signin link with hookup for our github client id

--- apps/codrspace/urls.py
+++ apps/codrspace/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('codrspace',                  
     url(r'^$', 'views.index', name="home_base"),
+    url(r'^signin/$', 'views.signin_start', name="signin_start"),
+    url(r'^signin_callback/$', 'views.signin_callback', name="signin_callback"),
 )
