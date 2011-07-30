5ea72066ee302cd9a4ed682f9daaf91b40f4c1b7
glenbot <glenbot@gmail.com>
2011-07-29 23:19:46 -0500

    More pep8 work

--- apps/codrspace/urls.py
+++ apps/codrspace/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
 
-urlpatterns = patterns('codrspace',        
+urlpatterns = patterns('codrspace',
     url(r'^$', 'views.index', name="home_base"),
 )
--- apps/codrspace/views.py
+++ apps/codrspace/views.py
@@ -3,5 +3,5 @@ from django.template import RequestContext
 
 
 def index(request, slug=None, template_name="base.html"):
-	return render_to_response(template_name, {},
-	    context_instance=RequestContext(request))
+    return render_to_response(template_name, {},
+        context_instance=RequestContext(request))
