694b98fc50fd3b7ad96d8adc75ad3ffd6bf45c34
Luke Lee <durdenmisc@gmail.com>
2011-07-30 00:09:36 -0500

    Remove extra views

--- apps/codrspace/urls.py
+++ apps/codrspace/urls.py
@@ -5,6 +5,5 @@ urlpatterns = patterns('codrspace.views',
     url(r'^$', 'index', name="home_base"),
     url(r'^edit/$', 'edit', name="edit"),
     url(r'^signin/$', 'signin_start', name="signin_start"),
-    url(r'^signin_callback/$', 'views.signin_callback',
-        name="signin_callback"),
+    url(r'^signin_callback/$', 'signin_callback', name="signin_callback"),
 )
