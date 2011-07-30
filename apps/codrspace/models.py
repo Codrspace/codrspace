0f6512a4cdb03d9013ae550b5b8d29d49318cfbf
Eloy Zuniga Jr <eloyz.email@gmail.com>
2011-07-29 23:09:05 -0500

    Checkout that model ... she's sexy.

--- apps/codrspace/models.py
+++ apps/codrspace/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from base.fields import SlugField
 
 class CodrSpace(models.Model):
-	slug = SlugField(unique=True)
-	title = models.CharField(max_length=200, blank=True)
-	content = models.TextField()
+    slug = SlugField(unique=True)
+    title = models.CharField(max_length=200, blank=True)
+    content = models.TextField()
     create_dt = models.DateTimeField(auto_now_add=True)
     update_dt = models.DateTimeField(auto_now=True)
