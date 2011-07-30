ce6bbe93ff091b7436d6b06f7342502e14060186
Luke Lee <durdenmisc@gmail.com>
2011-07-29 23:34:49 -0500

    Remove merge conflicts

--- apps/codrspace/views.py
+++ apps/codrspace/views.py
@@ -1,20 +1,11 @@
-<<<<<<< HEAD
-from django.shortcuts import render
-=======
 """Main codrspace views"""
 
-from django.shortcuts import render_to_response, redirect
->>>>>>> ce96d1c209bb5f2d3359681c7fac7d8bd3fceb84
-from django.template import RequestContext
+from django.shortcuts import render, redirect
 from settings import GITHUB_CLIENT_ID
 
 
 def index(request, slug=None, template_name="base.html"):
-<<<<<<< HEAD
     return render(request, template_name)
-=======
-    return render_to_response(template_name, {},
-        context_instance=RequestContext(request))
 
 
 def signin_start(request, slug=None, template_name="signin.html"):
@@ -25,9 +16,4 @@ def signin_start(request, slug=None, template_name="signin.html"):
 
 def signin_callback(request, slug=None, template_name="base.html"):
     """Callback from Github OAuth"""
-<<<<<<< HEAD
-    return render_to_response(template_name, {}, )
->>>>>>> ce96d1c209bb5f2d3359681c7fac7d8bd3fceb84
-=======
     print request.GET
->>>>>>> 5ccdd9fa87aabf074ad18261f14d2207e99402d7
