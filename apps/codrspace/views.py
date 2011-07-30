f31395d7d889a175c8fc509c2842f3b518c35b97
Luke Lee <durdenmisc@gmail.com>
2011-07-30 01:06:44 -0500

    Correctly identify github user login after oauth dance

--- apps/codrspace/views.py
+++ apps/codrspace/views.py
@@ -1,6 +1,7 @@
 """Main codrspace views"""
 
 from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import simplejson
 from settings import GITHUB_CLIENT_ID
 from codrspace.models import CodrSpace
 from codrspace.forms import CodrForm
@@ -44,6 +45,13 @@ def signin_start(request, slug=None, template_name="signin.html"):
                     GITHUB_CLIENT_ID))
 
 
+def _validate_github_response(resp):
+    """Raise exception if given response has error"""
+    if resp.status_code != 200 or 'error' in resp.content:
+        raise Exception('code: %u content: %s' % (resp.status_code,
+                                                  resp.content))
+
+
 def signin_callback(request, slug=None, template_name="base.html"):
     """Callback from Github OAuth"""
 
@@ -55,12 +63,16 @@ def signin_callback(request, slug=None, template_name="base.html"):
                                 '2b40ac4251871e09441eb4147cbd5575be48bde9',
                             'code': code})
     # FIXME: Handle error
-    if resp.status_code != 200 or 'error' in resp.content:
-        raise Exception('code: %u content: %s' % (resp.status_code,
-                                                  resp.content))
+    _validate_github_response(resp)
 
     # FIXME: Awful parsing w/ lots of assumptions
     # String looks like this currently
     # access_token=1c21852a9f19b685d6f67f4409b5b4980a0c9d4f&token_type=bearer
     token = resp.content.split('&')[0].split('=')[1]
-    return redirect('http://www.codrspace.com/%s' % (token))
+    resp = requests.get('https://api.github.com/user?access_token=%s' % (
+                                                                        token))
+    # FIXME: Handle error
+    _validate_github_response(resp)
+    user = simplejson.loads(resp.content)
+
+    return redirect('http://www.codrspace.com/%s' % (user['login']))
