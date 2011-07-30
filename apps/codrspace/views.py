ce96d1c209bb5f2d3359681c7fac7d8bd3fceb84
Luke Lee <durdenmisc@gmail.com>
2011-07-29 23:22:42 -0500

    pep8

--- apps/codrspace/views.py
+++ apps/codrspace/views.py
@@ -9,10 +9,12 @@ def index(request, slug=None, template_name="base.html"):
     return render_to_response(template_name, {},
         context_instance=RequestContext(request))
 
+
 def signin_start(request, slug=None, template_name="signin.html"):
     """Start of OAuth signin"""
     return redirect('https://github.com/login/oauth/authorize?client_id=%s' % (
                     GITHUB_CLIENT_ID))
+
 
 def signin_callback(request, slug=None, template_name="base.html"):
     """Callback from Github OAuth"""
