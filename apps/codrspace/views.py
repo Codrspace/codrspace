133457d37acd374a66cd2d26e83dd3075acd73c2
Eloy Zuniga Jr <eloyz.email@gmail.com>
2011-07-29 23:26:10 -0500

    Replaced old render method with new render method.  Added some Google Analytics.

--- apps/codrspace/views.py
+++ apps/codrspace/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.template import RequestContext
 
 
 def index(request, slug=None, template_name="base.html"):
-    return render_to_response(template_name, {},
-        context_instance=RequestContext(request))
+    return render(request, template_name)
