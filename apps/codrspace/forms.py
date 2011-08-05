c15015f0653516b5442dd2aefaf831ab78570126
glenbot <glenbot@gmail.com>
2011-08-04 21:21:44 -0500

    Added wmd markdown bar to help with the markdown

--- apps/codrspace/forms.py
+++ apps/codrspace/forms.py
@@ -3,12 +3,14 @@ from codrspace.models import Post, Media
 
 
 class PostForm(forms.ModelForm):
-
+    content = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'wmd-input'
+        }))
     class Meta:
         model = Post
 
 
 class MediaForm(forms.ModelForm):
-
     class Meta:
         model = Media
