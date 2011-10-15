e8bc242c154fe66ec9997f656ea1a4f3038e61f8
glenbot <glenbot@gmail.com>
2011-10-14 20:10:24 -0500

    RSS feed for post list page (closes #7)

---
+++ apps/codrspace/feeds.py
@@ -0,0 +1,30 @@
+from django.contrib.syndication.views import Feed
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from codrspace.models import Post
+
+
+class LatestPostsFeed(Feed):
+    description_template = 'feeds/post_description.html'
+
+    def get_object(self, request, username):
+        return get_object_or_404(User, username=username)
+
+    def title(self, obj):
+        return 'Codrspace: Posts by %s' % obj
+
+    def link(self, obj):
+        return '/%s/feed/' % obj
+
+    def description(self, obj):
+        return "Codrspace: All posts authored by %s" % obj
+
+    def items(self, user):
+        posts = Post.objects.filter(author=user, status="published")
+        return posts.order_by('-create_dt')[:10]
+
+    def item_title(self, item):
+        return item.title or 'Untitled'
+
+    def item_description(self, item):
+        return item.content
